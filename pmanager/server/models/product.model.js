5b04911a196f036faf063d11b1b9e8a8aac90b75
aRohBot <100891141+aRohBot@users.noreply.github.com>
2022-10-12 16:20:11 -0700

    Delete pmanager directory

--- pmanager/server/models/product.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const ProductSchema = new mongoose.Schema({
-    title: { type: String },
-    description: { type: String },
-    price: { type: Number }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Product', ProductSchema);
